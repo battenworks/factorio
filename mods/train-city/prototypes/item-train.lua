a34007ded56efc562d87a2cbfa2805f764f768fc
battenworks <djb@battenworks.com>
2021-12-19 08:26:22 -0700

    remove version from directory name

---
+++ mods/train-city/prototypes/item-train.lua
@@ -0,0 +1,26 @@
+local prototype_name = "tc-item-train"
+
+local entity = table.deepcopy(data.raw["locomotive"]["locomotive"])
+entity.name = prototype_name
+entity.localised_name = {"entity-name." .. prototype_name}
+
+local item = table.deepcopy(data.raw["item-with-entity-data"]["locomotive"])
+item.name = prototype_name
+item.localised_name = {"item-name." .. prototype_name}
+
+local recipe = table.deepcopy(data.raw["recipe"]["locomotive"])
+recipe.name = prototype_name
+recipe.localised_name = {"recipe-name." .. prototype_name}
+recipe.enabled = true
+recipe.ingredients = {
+	{"locomotive", 1},
+	{"advanced-circuit", 2}
+}
+recipe.placeresult = prototype_name
+recipe.result = prototype_name
+
+data:extend{
+  entity,
+	item,
+	recipe
+}
