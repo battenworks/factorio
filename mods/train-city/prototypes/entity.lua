935fa7d1b2a86678e748bc54d4d7a7a386c56a9b
battenworks <djb@battenworks.com>
2021-12-20 17:29:35 -0700

    entities give themselves back when mined

--- mods/train-city/prototypes/entity.lua
+++ mods/train-city/prototypes/entity.lua
@@ -3,6 +3,7 @@ function create_entity(name, from_type, from_item)
 	local entity = table.deepcopy(data.raw[from_type][from_item])
 	entity.localised_description = {"entity-description." .. name}
 	entity.localised_name = {"entity-name." .. name}
+	entity.minable = {mining_time = 1, result = name}
 	entity.name = name
 
 	return entity
