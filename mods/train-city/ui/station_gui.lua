3d5ba1400d329c2edc36620919d6f36b583548c3
battenworks <djb@battenworks.com>
2022-02-25 16:37:53 -0700

    bug fix: fluid station circuit condition

--- mods/train-city/ui/station_gui.lua
+++ mods/train-city/ui/station_gui.lua
@@ -43,7 +43,39 @@ local function find_all_trains_associated_with_station(station_name)
 	return associated_trains
 end
 
-local function build_circuit_condition(selected_item_name, selected_direction)
+local function build_fluid_circuit_condition(selected_fluid_name, selected_direction)
+	local fluid_prototype = game.fluid_prototypes[selected_fluid_name]
+
+	if fluid_prototype == nil then
+		return nil
+	end
+
+	local cargo_train_capacity = 50000
+	local condition_comparator
+	local condition_constant
+
+	if selected_direction == "load" then
+		condition_comparator = ">"
+		condition_constant = cargo_train_capacity
+	else
+		condition_comparator = "<"
+		local total_capacity = 150000
+		condition_constant = total_capacity - cargo_train_capacity
+	end
+
+	return {
+		condition = {
+			comparator = condition_comparator,
+			first_signal = {
+				type = "fluid",
+				name = selected_fluid_name,
+			},
+			constant = condition_constant,
+		}
+	}
+end
+
+local function build_item_circuit_condition(selected_item_name, selected_direction)
 	local item_prototype = game.item_prototypes[selected_item_name]
 
 	if item_prototype == nil then
@@ -76,10 +108,18 @@ local function build_circuit_condition(selected_item_name, selected_direction)
 	}
 end
 
-local function set_new_train_station_configuration(train_station, selected_item_name, selected_direction)
+local function build_circuit_condition(selected_item_name, item_type, selected_direction)
+	if item_type == "fluid" then
+		return build_fluid_circuit_condition(selected_item_name, selected_direction)
+	else
+		return build_item_circuit_condition(selected_item_name, selected_direction)
+	end
+end
+
+local function set_new_train_station_configuration(train_station, selected_item_name, item_type, selected_direction)
 	local control_behavior = train_station.get_or_create_control_behavior()
 	control_behavior.enable_disable = true
-	control_behavior.circuit_condition = build_circuit_condition(selected_item_name, selected_direction)
+	control_behavior.circuit_condition = build_circuit_condition(selected_item_name, item_type, selected_direction)
 
 	train_station.backer_name = selected_item_name .. " " .. selected_direction
 	train_station.trains_limit = 1
@@ -190,9 +230,9 @@ station_gui.configure_train_station = function (player, item_type)
 			old_schedule = associated_trains[1].schedule
 		end
 
-		set_new_train_station_configuration(train_station, selected_item_name, selected_direction)
+		set_new_train_station_configuration(train_station, selected_item_name, item_type, selected_direction)
 		reassociate_trains_with_old_schedule(associated_trains, old_schedule)
 	else
-		set_new_train_station_configuration(train_station, selected_item_name, selected_direction)
+		set_new_train_station_configuration(train_station, selected_item_name, item_type, selected_direction)
 	end
 end
