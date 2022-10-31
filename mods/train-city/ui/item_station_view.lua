4b8abfecb5a03b275891c0a38b79686c0df9e546
battenworks <djb@battenworks.com>
2022-10-31 07:35:27 -0600

    rename views

--- mods/train-city/ui/item_station_view.lua
+++ mods/train-city/ui/item_station_view.lua
@@ -2,8 +2,8 @@ require("ui.station_view")
 
 local item_type = "item"
 local view_name = "bwtc_item_station_view"
-local selection_button_name = "bwtc_item_station_selection_button"
-local direction_switch_name = "bwtc_item_station_direction_switch"
+local selection_button_name = "bwtc_item_station_view_selection_button"
+local direction_switch_name = "bwtc_item_station_view_direction_switch"
 
 item_station_view = {
 	name = view_name,
@@ -11,14 +11,14 @@ item_station_view = {
 	direction_switch_name = direction_switch_name,
 }
 
-item_station_view.toggle = function (player, entity)
+item_station_view.toggle = function(player, entity)
 	station_view.toggle(player, entity, item_type, view_name, selection_button_name, direction_switch_name)
 end
 
-item_station_view.clear = function (player)
+item_station_view.clear = function(player)
 	station_view.clear(player)
 end
 
-item_station_view.configure_train_station = function (player)
+item_station_view.configure_train_station = function(player)
 	station_view.configure_train_station(player, item_type)
 end
